366d6bc9eb0023ab7a48c9d3c47f5c9999190994
刘文祥 <liuwenxiang@liuwenxiangdeMacBook-Pro.local>
2020-06-27 22:05:46 +0800

    modify .vuepress-config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,50 +1,91 @@
+const interviewConfig = {
+  path: '/interview/',
+  title: '前端面试题整理',
+  collapsable: true, // 可选的, 默认值是 true,
+  sidebarDepth: 3,    // 可选的, 默认值是 1
+  children: [
+    {
+      title: 'shopLine',
+      path: 'interview/shopLine'
+    },
+    {
+      title: '探迹',
+      path: 'interview/探迹'
+    },
+    {
+      title: 'vip',
+      path: 'interview/vip'
+    },
+    {
+      title: '翼课网',
+      path: 'interview/翼课网'
+    },
+    {
+      title: '有米科技',
+      path: 'interview/有米科技'
+    },
+    {
+      title: 'PPMoney',
+      path: 'interview/PPMoney'
+    }
+  ]
+}
+const vueCodeStudyConfig = {
+  path: '/vue-code-stutdy/',
+  title: 'Vue源码学习',
+  collapsable: true, // 可选的, 默认值是 true,
+  sidebarDepth: 3,    // 可选的, 默认值是 1
+}
+
+const LifeArticlesConfig = {
+  path: '/life-articles/',
+  title: '生活中的零零碎碎',
+  collapsable: true, // 可选的, 默认值是 true,
+  sidebarDepth: 3,    // 可选的, 默认值是 1
+}
+
+const configArr = [
+  interviewConfig,
+  vueCodeStudyConfig,
+  LifeArticlesConfig
+]
+
+
+const navAriaLabelFromConfig = (function (configArr) {
+  return configArr.reduce((pre, cur) => {
+    let { path: link, title: text } = cur
+    return [...pre, {
+      text,
+      link: `${link}index`
+    }]
+  }, []);
+})(configArr)
+
+
+
 module.exports = {
   dest: './docs/.vuepress/dist/',
   base: '/',
   title: 'LauwXiang Blog',
   description: '前端',
   themeConfig: {
-    // sidebar: 'auto'
-    sidebar: [
-      {
-        title: '前端面试题整理',
-        path: '/interview/',
-        collapsable: true, // 可选的, 默认值是 true,
-        sidebarDepth: 3,    // 可选的, 默认值是 1
-        children: [
-          {
-            title: 'shopLine',
-            path: 'interview/shopLine'
-          },
-          {
-            title: '探迹',
-            path: 'interview/探迹'
-          },
-          {
-            title: 'vip',
-            path: 'interview/vip'
-          },
-          {
-            title: '翼课网',
-            path: 'interview/翼课网'
-          },
-          {
-            title: '有米科技',
-            path: 'interview/有米科技'
-          },
-          {
-            title: 'PPMoney',
-            path: 'interview/PPMoney'
-          }
-        ]
-      },
-      {
-        title: 'vue底层源码学习',
-        path: '/vue-code-stutdy/',
-        collapsable: true, // 可选的, 默认值是 true,
-        sidebarDepth: 3,    // 可选的, 默认值是 1
-      }
+    //顶部导航栏
+    nav: [
+      //格式一：直接跳转，'/'为不添加路由，跳转至首页
+      { text: '首页', link: '/' },
 
-    ]
+      //格式二：添加下拉菜单，link指向的文件路径
+      {
+        text: '分类',  //默认显示        
+        ariaLabel: '分类',   //用于识别的label
+        items: navAriaLabelFromConfig
+
+      },
+
+      //格式三：跳转至外部网页，需http/https前缀
+      { text: 'Github', link: 'https://github.com/LauwXiang/' },
+    ],
+    // sidebar: 'auto'
+    sidebar: configArr
   }
 }
